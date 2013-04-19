8a2a8e333c459de894a8bee8b380a362f2a00568
SomeKittens <rkoutnik@gmail.com>
2013-04-18 21:17:57 -0400

    added input box

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -1,9 +1,8 @@
 !function() {
   'use strict';
   
-  //Set up our syntax-highlighting goodness CodeMirror
-  var input = document.getElementById('pyCode');
-  var pyCode = CodeMirror.fromTextArea(input, {
+    //Set up our syntax-highlighting goodness CodeMirror
+  var codeMirrorConfig = {
     value: 'print("hello world")',
     mode: {
       name: 'python',
@@ -12,13 +11,21 @@
       },
     lineNumbers: true,
     lineWrapping: true
-  });
+  }
+    , code = document.getElementById('pyCode')
+    , input = document.getElementById('pyInput');
+  
+  var pyCode = CodeMirror.fromTextArea(code, codeMirrorConfig)
+    , pyInput = CodeMirror.fromTextArea(input, codeMirrorConfig);
+  
+  //HCI
+  pyInput.setSize(700, 130);
   
   //Set up POST form submission
   $('#run').click(function(e) {
     $.post('/python', {
       code: pyCode.getValue(),
-      input: $('#pyInput').val()
+      input: pyInput.getValue()
     }, function(data) {
       console.log(data);
       $('#results').text(data);
